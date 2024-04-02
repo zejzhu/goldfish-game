6cbf7c89c885d109ecd92ef9b6e14fe46279c8dd
dlinbeck <142837109+dlinbeck@users.noreply.github.com>
2024-04-02 03:21:11 -0400

    Textboxes added

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+var kelpCount = 0;
+
+// INTERACT WITH OBJECTS
 AFRAME.registerComponent('interact-highlight', {
     init: function () {
       // Wait for model to load.
       this.el.addEventListener('model-loaded', () => {
         this.el.addEventListener('mouseenter', this.mouseenter);
         this.el.addEventListener('mouseleave', this.mouseleave);
-        this.el.addEventListener('click', this.click);
       })
     },
     mouseenter(evt) { //on hover, do this
@@ -23,16 +25,42 @@ AFRAME.registerComponent('interact-highlight', {
     }
 });
 
+// TRIGGER EVENTS WITH CLICKS
 AFRAME.registerComponent('click-kelp', { //When the kelp is clicked
     init: function () {
       // Wait for model to load.
+      console.log(kelpCount);
       this.el.addEventListener('model-loaded', () => {
         this.el.addEventListener('click', this.click);
       })
     },
     click(evt){
-        //do stuff here when kelp clicked
-        console.log("clicked");
+        console.log("clicked kelp");
+        kelpCount++;
+        holder = kelpCount;
+        console.log(holder);
+
+        var fog = document.getElementById('purp-fog');
+        if(holder == 1){
+          var textbox = document.getElementById('kelp-1');
+          textbox.object3D.visible = true;
+          fog.setAttribute('far', '90'); //fog enters
+        }
+        if(holder == 2){
+          var textbox = document.getElementById('kelp-1');
+          textbox.object3D.visible = true;
+          fog.setAttribute('far', '30'); //fog strong
+        }
+        if(holder == 3){
+          //transition to kelp death scene - KEVIN
+            //flush scene into kelp death scene
+        }
+
+        document.addEventListener('keydown', function(event){ 
+          if (event.key === 'h'){
+            textbox.object3D.visible = false;
+          }
+        })
     }
 });
 
@@ -45,11 +73,87 @@ AFRAME.registerComponent('click-note', { //When the note is clicked
     },
     click(evt){
         //do stuff here when note clicked
-        console.log("clicked");
+        console.log("clicked note");
+        //show textbox
+        var textbox = document.getElementById('note-1');
+        textbox.object3D.visible = true;
+
+        document.addEventListener('keydown', function(event){ 
+          if (event.key === 'h'){
+            textbox.object3D.visible = false;
+          }
+        })
     }
 });
 
-AFRAME.registerComponent('rotation-reader', { // Camera
+//one min
+AFRAME.registerComponent('one-min', { 
+  init: function () {
+    // Wait for model to load.
+    var fog = document.getElementById('green-fog');
+    this.el.addEventListener('model-loaded', () => {
+      var textbox = document.getElementById('you-1');
+      var next = document.getElementById('nar-2');
+      var delayInMilliseconds = 60000; //60 seconds
+      var count = 0;
+
+      setTimeout(function() {
+          textbox.object3D.visible = true; //code to be executed after 60 seconds
+          fog.setAttribute('far', '90'); //fog enters
+        }, delayInMilliseconds);
+        
+      document.addEventListener('keydown', function(event){ 
+        if (event.key === 'h'){
+          textbox.object3D.visible = false;
+          if(count == 0){
+            next.object3D.visible = true;
+            count++;
+          }
+          textbox = next;
+        }
+      })
+    })
+  }
+});
+//two min
+AFRAME.registerComponent('two-min', { 
+  init: function () {
+    // Wait for model to load.
+    var fog = document.getElementById('green-fog');
+    this.el.addEventListener('model-loaded', () => {
+      var textbox = document.getElementById('you-3');
+      var delayInMilliseconds = 120000; //120 seconds
+
+      setTimeout(function() {
+          textbox.object3D.visible = true; //code to be executed after 120 seconds
+          fog.setAttribute('far', '30'); //fog enters
+        }, delayInMilliseconds);
+        
+      document.addEventListener('keydown', function(event){ 
+        if (event.key === 'h'){
+          textbox.object3D.visible = false;
+        }
+      })
+    })
+  }
+});
+//three min
+AFRAME.registerComponent('three-min', { 
+  init: function () {
+    // Wait for model to load.
+    this.el.addEventListener('model-loaded', () => {
+      var delayInMilliseconds = 180000; //180 seconds
+
+      setTimeout(function() {
+          //algae death transition goes here  - KEVIN
+          //code here will be executed after 3 min
+        }, delayInMilliseconds);
+      })
+    }
+  });
+
+// CAMERA ROTATION
+AFRAME.registerComponent('rotation-reader', { 
 
     tick: (function () {
       var position = new THREE.Vector3();

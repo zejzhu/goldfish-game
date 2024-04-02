f8bfbd0945a3839e530907fa05dba31c3a2d0401
Shaniah Reece <104451377+reecey22@users.noreply.github.com>
2024-04-01 23:34:38 -0400

    Update rotateCam.js

--- interactionCode/rotateCam.js
+++ interactionCode/rotateCam.js
@@ -1,12 +1,24 @@
-AFRAME.registerComponent('rotation-reader, {
+const AFRAME = window.AFRAME;
+AFRAME.registerComponent('rotation-reader', {
     tick: function() {
+        var pinkBox = document.getElementByID('pinkBox')
+        var plane = document.getElementByID('plane')
         var rotation = this.el.getAttribute('rotation');
-        if (rotation.y >= 45) {
+        var colors = ['#FF0000', '#00FF00', '#0000FF',  
+                    '#FFFF00']; // List of colors
+        var cam = document.getElementByID('cam')
+        var randomColor = colors[Math.floor(Math.random() * colors.length)];
+      
+        if (rotation.y > 0)  {
             print('You made it out')
-            background-color = "blue"
+            pinkBox.setAttribute('color', randomColor)
+            plane.setAttribute('color', 'blue')
         }
+      if (rotation.x > 0)  {
+            print('You made it out')
+            pinkBox.setAttribute('color', randomColor)
+            plane.setAttribute('color', 'blue')
+        }
+      
     }
 });
-
-
-//stackoverflow: how to listen to changes in the rotation of the camera in A-frame?

68e2b8d69e44cb88d17292d2bc1c73305a04a12b
Shaniah Reece <104451377+reecey22@users.noreply.github.com>
2024-04-02 01:36:58 -0400

    Fixed rotateCam.js !
    Sphere changes color after 5 second delay; to be changed to change scene after delay:)

--- interactionCode/rotateCam.js
+++ interactionCode/rotateCam.js
@@ -1,24 +1,22 @@
 const AFRAME = window.AFRAME;
 AFRAME.registerComponent('rotation-reader', {
     tick: function() {
-        var pinkBox = document.getElementByID('pinkBox')
-        var plane = document.getElementByID('plane')
+        var pinkBox = document.getElementById('pinkBox')
+        var plane = document.getElementById('plane')
         var rotation = this.el.getAttribute('rotation');
-        var colors = ['#FF0000', '#00FF00', '#0000FF',  
-                    '#FFFF00']; // List of colors
-        var cam = document.getElementByID('cam')
+        var colors = ['#FF0000', '#00FF00', '#0000FF',  '#FFFF00']; // List of colors
         var randomColor = colors[Math.floor(Math.random() * colors.length)];
+      var delayInMilliseconds = 5000; //5 seconds
       
-        if (rotation.y > 0)  {
-            print('You made it out')
-            pinkBox.setAttribute('color', randomColor)
-            plane.setAttribute('color', 'blue')
-        }
-      if (rotation.x > 0)  {
-            print('You made it out')
-            pinkBox.setAttribute('color', randomColor)
-            plane.setAttribute('color', 'blue')
-        }
-      
+        if (rotation.x > 45)  {
+            console.log('You made it out')
+          setTimeout(function() {pinkBox.setAttribute('color', 'blue');}, delayInMilliseconds);
+        } 
+            
+      else {
+          console.log ('Oops try again')
+          pinkBox.setAttribute('color', '#FF00FF');
+          plane.setAttribute('color', '#4b5320');
+        }    
     }
 });
